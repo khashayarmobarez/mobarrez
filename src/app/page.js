656ef2a1259ca028e8a9579b950ca777ae82cf7a
khashayar <khashayarmobarezbusiness@gmail.com>
2025-03-25 18:45:48 +0330

    refactor: replace default page with Landing component and add AtomicLoader

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-import Image from "next/image";
+import Landing from "@/templates/Landing";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-y-2">
-      <h1 className=" text-3xl">Mobarrez</h1>
-      <h className=" text-lg">Precision in Progress</h>
-      <p className=" text-center mt-2 w-2/3 md:w-2/5">Mobarrez delivers precision-engineered web solutions, bold branding, and smart marketing strategies to drive business growth—using a modern tech stack (Next.js, React, serverless) and a disciplined approach to create cohesive, impactful identities and results.</p>
-    </div>
+    <Landing />
   );
 }
---
+++ src/templates/Landing.jsx
@@ -0,0 +1,20 @@
+import AtomicLoader from '@/elements/AtomicLoader';
+import React from 'react';
+
+const Landing = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen gap-y-2">
+            <h1 className=" text-3xl font-extrabold">Mobarrez</h1>
+            <h className=" text-lg font-medium mb-4">Precision in Progress</h>
+            <AtomicLoader />
+            <div className='flex flex-col items-center justify-center w-2/3 md:w-2/5'>
+                <p className=" text-center mt-2 text-lg">App’s Coming</p>
+                <p className=" text-center text-base">Precision Takes Time</p>
+            </div>
+        </div>
+    );
+
+};
+
+export default Landing;
