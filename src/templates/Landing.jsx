efad151c3935130755d553efdb7829f30614f8dd
khashayar <khashayarmobarezbusiness@gmail.com>
2025-03-26 22:25:23 +0330

    feat: add contact information section to Landing component

--- src/templates/Landing.jsx
+++ src/templates/Landing.jsx
@@ -12,6 +12,15 @@ const Landing = () => {
                 <p className=" text-center mt-2 text-lg">App’s Coming</p>
                 <p className=" text-center text-base"><span className='text-accent'>Precision</span> Takes Time</p>
             </div>
+            <div className='flex flex-col items-center justify-center w-2/3 md:w-2/5'>
+                <p className=" text-center mt-2 text-lg">contact us</p>
+                <a 
+                href="mailto:khashayarmobarezbusiness@gmail.com" 
+                className=" text-center text-base text-accent hover:underline"
+                >
+                    khashayarmobarezbusiness@gmail.com
+                </a>
+            </div>
         </div>
     );
 
