790f6981eb1d5bedf4de846dd55c9b1abf0c8bdf
khashayar <khashayarmobarezbusiness@gmail.com>
2025-04-14 20:26:02 +0330

    fix: change header element from h to h2 for semantic correctness in Landing component

--- src/components/templates/Landing.jsx
+++ src/components/templates/Landing.jsx
@@ -31,7 +31,7 @@ const Landing = () => {
                 <MorphingText texts={["Mobarrez", "AI automation", "Web-apps", "Applications"]} />
             </div>
             {/* <h1 className=" text-3xl font-extrabold">Mobarrez</h1> */}
-            <h className=" text-lg font-medium mb-0 -mt-5 md:mt-2">Precision in Progress</h>
+            <h2 className=" text-lg font-medium mb-0 -mt-5 md:mt-2">Precision in Progress</h2>
             {/* <AtomicLoader /> */}
             {/* <div className='flex flex-col items-center justify-center w-2/3 md:w-2/5'>
                 <p className=" text-center mt-2 text-lg">App’s Coming</p>
