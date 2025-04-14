17e64fc31528306c95c653ade6c0eb960598b9cf
khashayar <khashayarmobarezbusiness@gmail.com>
2025-04-14 18:54:32 +0330

    feat: add meteor animation to Landing component and update global styles for enhanced visuals

--- src/components/templates/Landing.jsx
+++ src/components/templates/Landing.jsx
@@ -1,11 +1,15 @@
 import Logo from '@/assets/logo/Logo';
 import AtomicLoader from '@/components/elements/AtomicLoader';
 import React from 'react';
+import { Meteors } from '../magicui/meteors';
 
 const Landing = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen gap-y-2">
+            <div className="relative overflow-hidden h-[500px] w-full max-w-[350px] md:max-w-none -mb-[450px] -mt-[150px] ">
+                <Meteors />
+            </div>
             <Logo className="w-28 h-20 -mb-4" color='var(--foreground)' />
             <h1 className=" text-3xl font-extrabold">Mobarrez</h1>
             <h className=" text-lg font-medium mb-0">Precision in Progress</h>
