2b75164409bc537a53e4b1608e773458ee891d81
khashayar <khashayarmobarezbusiness@gmail.com>
2025-04-18 14:02:15 +0330

    feat: update AboutSection to use new icon components for improved modularity and styling

--- src/components/modules/landing/AboutSection.jsx
+++ src/components/modules/landing/AboutSection.jsx
@@ -1,17 +1,24 @@
+'use client';
+
 import React from 'react';
 
 // components
 import { IconCloud } from '@/components/magicui/icon-cloud';
 
 // icons
-import ReactIcon from '@/assets/iconCloudIcons/react-2.svg'
-import NextIcon from '@/assets/iconCloudIcons/next-js.svg'
+import NextIcon from '@/components/icons/NextIcon';
+import TailwindIcon from '@/components/icons/TailwindIcon';
+import FirebaseIcon from '@/components/icons/FirebaseIcon';
 
 const AboutSection = () => {
     return (
         <div className='flex flex-col w-full h-[90vh] items-center gap-y-2 snap-section'>
             <div className='w-auto'>
-                <IconCloud />
+                <IconCloud icons={[
+                    <NextIcon key="next" className="text-foreground" />,
+                    <TailwindIcon key="tailwind" className="text-[#06B6D4]" />,
+                    <FirebaseIcon key="firebase" />
+                ]} />
             </div>
         </div>
     );
