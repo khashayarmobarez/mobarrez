4170adb7daafec34820ebf246339cf0188f2a0bc
khashayar <khashayarmobarezbusiness@gmail.com>
2025-04-20 20:36:15 +0330

    feat: enhance AboutSection layout with new card component and styling for improved visual appeal

--- src/components/modules/landing/AboutSection.jsx
+++ src/components/modules/landing/AboutSection.jsx
@@ -22,7 +22,24 @@ import GoogleCloudIcon from '@/components/icons/GoogleCloudIcon';
 
 const AboutSection = () => {
     return (
-        <div className='flex flex-col w-full h-[90vh] items-center gap-y-2 snap-section'>
+        <div className='flex w-full h-[100vh] pt-16 items-center justify-between gap-y-2 snap-section'>
+            
+            <div className="card">
+                <div className="tools">
+                    <div className="circle">
+                        <span className="red box" />
+                    </div>
+                    <div className="circle">
+                        <span className="yellow box" />
+                    </div>
+                    <div className="circle">
+                        <span className="green box" />
+                    </div>
+                </div>
+                <div className="card__content">
+                </div>
+            </div>
+
             <div className='w-auto'>
                 <IconCloud icons={[
                     <NextIcon key="next" className="text-foreground" />,
@@ -47,8 +64,50 @@ const AboutSection = () => {
                     <GoogleBardIcon key="google-bard" />,
                     <GoogleCloudIcon key="google-cloud" />,
                     <GoogleCloudIcon key="google-cloud" />
-                ]} />
+                ]}/>
             </div>
+
+            <style jsx>{`
+                .card {
+                    width: 40%;
+                    height: 404px;
+                    margin: 0 auto;
+                    background-color: #011522;
+                    border-radius: 8px;
+                    z-index: 1;
+                }
+
+                .tools {
+                    display: flex;
+                    align-items: center;
+                    padding: 9px;
+                }
+
+                .circle {
+                    padding: 0 4px;
+                }
+
+                .box {
+                    display: inline-block;
+                    align-items: center;
+                    width: 10px;
+                    height: 10px;
+                    padding: 1px;
+                    border-radius: 50%;
+                }
+
+                .red {
+                    background-color: #ff605c;
+                }
+
+                .yellow {
+                    background-color: #ffbd44;
+                }
+
+                .green {
+                    background-color: #00ca4e;
+                }
+            `}</style>
         </div>
     );
 };
