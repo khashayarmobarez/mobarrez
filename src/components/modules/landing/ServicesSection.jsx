14c25d5e08f0426dad9dec545476b791346cd3c9
khashayar <khashayarmobarezbusiness@gmail.com>
2025-05-11 16:46:53 +0330

    Refactor code structure for improved readability and maintainability

--- src/components/modules/landing/ServicesSection.jsx
+++ src/components/modules/landing/ServicesSection.jsx
@@ -1,11 +1,16 @@
 'use client';
-
 import React, { useEffect, useState } from 'react';
+
+// assets
+import consultingData from '@/../public/data-extraction-animate.svg';
+
+// comps
 import { TypingAnimation } from '@/components/magicui/typing-animation';
 import { AnimatedBeam } from '@/components/magicui/animated-beam';
 import AnimatedBeamDemo from './AiAutomationAnimate';
 import { Terminal } from '@/components/magicui/terminal';
 import { TerminalSection } from '@/components/elements/TerminalSection';
+import Image from 'next/image';
 
 const ServicesSection = () => {
     
@@ -16,7 +21,7 @@ const ServicesSection = () => {
     useEffect(() => {
         // Calculate vh in pixels
         const vhInPixels = window.innerHeight;
-        const thresholds = [2.1, 3.2, 4.1].map((factor) => vhInPixels * factor); // 200vh, 250vh, 270vh
+        const thresholds = [2.1, 3.2, 4.4].map((factor) => vhInPixels * factor); // 200vh, 250vh, 270vh
 
         const handleScroll = () => {
             const scrollPosition = window.scrollY + vhInPixels; // Bottom of viewport position
@@ -76,8 +81,8 @@ const ServicesSection = () => {
                             <div className='text-start w-full flex items-center  md:w-[60%] '>
                                 <TerminalSection />
                             </div>
-                            <p className=" text-xl md:w-[35%] font-semibold">
-                                We integrate advanced AI and automation to optimize your workflows with precision, delivering personalized solutions that boost efficiency and elevate customer experiences.
+                            <p className=" text-xl md:w-[35%] font-semibold -mt-4 md:-mt-0">
+                                We build custom, scalable software with cutting-edge tools, ensuring robust solutions tailored to your needs.
                             </p>
                         </div>
                     </>
@@ -86,9 +91,20 @@ const ServicesSection = () => {
             <div className='flex flex-col items-center text-center px-4 md:items-start justify-start pt-16 md:pl-16 w-full h-[100vh] bg-foreground text-background'>
                 {
                 visible3 && (
-                    <TypingAnimation className="text-4xl font-bold">
-                        Startups and consulting
-                    </TypingAnimation>
+                    <>
+                        <TypingAnimation className="text-4xl font-bold">
+                            Startups and consulting
+                        </TypingAnimation>
+                        <div className='flex flex-col items-start justify-around  w-full h-full  md:gap-y-8
+                        md:flex-row md:justify-between md:px-4 md:items-center'>
+                            <div className='text-start w-full flex items-center md:w-[40%] '>
+                                <Image src={consultingData} width={100} height={100} className='w-full' />
+                            </div>
+                            <p className=" text-2xl md:w-[35%] font-semibold">
+                            We guide startups with expert consulting and tech strategies, accelerating growth with precision and innovation.
+                            </p>
+                        </div>
+                    </>
                 )}
             </div>
         </div>
