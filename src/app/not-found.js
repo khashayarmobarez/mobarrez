f5ba4929341b4e2eef61d8d7d27ab996a8f8b204
khashayar <khashayarmobarezbusiness@gmail.com>
2025-05-13 16:42:45 +0330

    style: enhance NotFound component layout and update link styles for better UX

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ import NotFoundSSR from '@/components/modules/NotFoundSSR';
 
 export default function NotFound() {
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center bg-background snap-section overflow-hidden">
+    <div className="relative flex min-h-screen flex-col items-center justify-center md:pt-10 md:justify-start bg-background snap-section overflow-hidden">
       {/* Meteors background effect */}
       <Meteors number={20} className="z-0" />
 
--- src/components/modules/NotFoundSSR.jsx
+++ src/components/modules/NotFoundSSR.jsx
@@ -7,14 +7,14 @@ import Image from 'next/image';
 
 const NotFoundSSR = () => {
     return (
-        <div className="relative z-10 text-center w-3/4">
-            <Image src={NotFoundPicture} alt='not found' width={100} height={100} className='w-full'  />
+        <div className="relative z-10 text-center w-3/4 flex flex-col items-center h-full gap-y-4">
+            <Image src={NotFoundPicture} alt='not found' width={100} height={100} className='w-full md:w-1/3'  />
             <p className="text-lg text-muted-foreground mb-6">
-                Oops! It looks like the page you’re looking for doesn’t exist.
+                It looks like the page you’re looking for doesn’t exist.
             </p>
             <Link
             href="/"
-            className="inline-block rounded-2xl bg-accent px-6 py-3 text-background transition hover:bg-opacity-80"
+            className="inline-block rounded-2xl bg-none border border-accent px-6 py-3 text-accent transition hover:scale-105"
             >
             Return to Homepage
             </Link>
