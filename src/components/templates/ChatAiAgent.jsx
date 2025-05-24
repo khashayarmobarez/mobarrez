d443ea5b697e6e2d2c0808da66cd4ff068ac5842
khashayar <khashayarmobarezbusiness@gmail.com>
2025-05-24 20:46:47 +0330

    style: restore placeholder comment in ChatAiAgent component for clarity

--- src/components/templates/ChatAiAgent.jsx
+++ src/components/templates/ChatAiAgent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ChatAiAgent = () => {
     return (
         <div className='flex flex-col items-center justify-center w-full h-screen'>
-            
+            {/* not developed yet */}
         </div>
     );
 };
