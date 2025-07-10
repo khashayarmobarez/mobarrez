9bf0c655407bfaa758a7ded8171782405bf0d53a
khashayar <khashayarmobarezbusiness@gmail.com>
2025-07-10 12:29:20 +0330

    feat: implement MongoDB connection and adapter for NextAuth

--- auth.js
+++ auth.js
@@ -2,7 +2,11 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import LinkedIn from "next-auth/providers/linkedin"
 
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import client from "@/lib/db"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(client),
   // https://console.cloud.google.com/
   providers: [GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,
