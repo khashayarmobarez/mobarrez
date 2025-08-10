1973d172c9e5da6838162e53f106e6150cc1f108
khashayar <khashayarmobarezbusiness@gmail.com>
2025-08-10 15:26:52 +0330

    fix: update n8n progress in README and add scheduling request details in page.js

--- src/app/page.js
+++ src/app/page.js
@@ -6,3 +6,8 @@ export default function Home() {
     <Landing />
   );
 }
+
+
+{{
+  `Scheduling request: "${$node["Process Chat Message"].json.originalMessage}". Details: title=${$node["Process Chat Message"].json.scheduleData.title}, time=${$node["Process Chat Message"].json.scheduleData.timeExtracted || 'not specified'}, date=${$node["Process Chat Message"].json.scheduleData.dateExtracted || 'not specified'}. Confirm these details and ask for any additional information needed to schedule.`
+}}
