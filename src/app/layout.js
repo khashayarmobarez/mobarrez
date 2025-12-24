4f34c59388c2d1301bf3bb00c4db24d90281ffee
khashayar <khashayarmobarezbusiness@gmail.com>
2025-12-24 17:43:09 +0330

    fixed hydration error

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
-      <body className={``}>
+      <body >
         <SessionProviderWrapper>
           <Navbar />
           {children}
